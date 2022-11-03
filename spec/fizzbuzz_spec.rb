c2a47b47a6710d6db7ebf43e86af6dfe147bd70f
Chris <c.bailey00@hotmail.com>
2022-11-03 10:53:28 +0000

    created fizzbuzz.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,8 @@
-describe'fizzbuzz' do
-  it 'return "fizz" when passed 3'
-  expect(fizzbuzz(3)). to eq 'fizz'
-end
-end
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end 
+
